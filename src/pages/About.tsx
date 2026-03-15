86b3e2ecee3b23c5a271bfa3a07d91a3a60568a3
Roos Catling-Tate <roos@catling-tate.net>
2026-03-15 19:32:40 -0400

    Add about content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,15 +1,13 @@
 import PageContainer from "../components/PageContainer.tsx";
-import LoremIpsum from "../components/LoremIpsum.tsx";
 
 const About = () => {
     return (<>
         <PageContainer title={'About Us'}>
-            <LoremIpsum/>
-            <LoremIpsum/>
-            <LoremIpsum/>
-            <LoremIpsum/>
-            <LoremIpsum/>
-            <LoremIpsum/>
+            <p>Gladiator Farms started as a market garden in 2022 just outside Cornwall, Ontario in a place called Saint
+                Andrews’ West. We grow fruits and vegetables for hungry gladiators - everyday people who want to feel
+                healthier and stronger from the food they eat.</p>
+            <p>Our current expansion plans are underway for 2026, and we look forward to seeing you again this year as
+                we grow our operations.</p>
         </PageContainer>
     </>);
 };
