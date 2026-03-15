964648e3ddf07a578f2a44465ea6a6212fa3ebda
Roos Catling-Tate <roos@catling-tate.net>
2026-03-15 11:30:20 -0400

    Add home bg image overlay

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ function Home() {
     return (<>
         <div className={'home-bg-container'}>
             <Image className={'home-bg'} src={'main-bg.jpeg'}/>
+            <Image className={'home-bg-overlay'} src={'gladiator-farms.png'}/>
         </div>
     </>);
 }
