f6dffe6c5d3c9c54999d834294d2659d23c8adbc
Roos Catling-Tate <roos@catling-tate.net>
2026-03-16 07:34:56 -0400

    Update about and add carousel

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,5 @@
 import PageContainer from "../components/PageContainer.tsx";
+import { Carousel } from "react-bootstrap";
 
 const About = () => {
     return (<>
@@ -6,8 +7,19 @@ const About = () => {
             <p>Gladiator Farms started as a market garden in 2022 just outside Cornwall, Ontario in a place called Saint
                 Andrews’ West. We grow fruits and vegetables for hungry gladiators - everyday people who want to feel
                 healthier and stronger from the food they eat.</p>
-            <p>Our current expansion plans are underway for 2026, and we look forward to seeing you again this year as
-                we grow our operations.</p>
+
+            <p>Our current expansion plans are underway for 2026, where we will be adding our all-season, state of the
+                art greenhouses with heating and lighting to help serve you better.</p>
+
+            <p>We look forward to seeing you again this year as we grow our operations.</p>
+            <Carousel fade className={'about-carousel'}>
+                <Carousel.Item>
+                    <img className={'about-carousel-img'} src={'about/about-1.jpeg'} alt={'The farm'}/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className={'about-carousel-img'} src={'about/about-2.jpeg'} alt={'The farm'}/>
+                </Carousel.Item>
+            </Carousel>
         </PageContainer>
     </>);
 };
